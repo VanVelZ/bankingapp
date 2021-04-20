a516811fce1e5b576c63b682f6231552729de2ee
VanVelZ <zachofasunset@me.com>
2021-04-20 15:46:40 -0500

    Finished account service tests for real

--- tests/account_service_tests.py
+++ tests/account_service_tests.py
@@ -2,45 +2,43 @@ import unittest
 from models.account import Account
 from services.account_service import AccountService
 
-unittest.TestLoader.sortTestMethodsUsing = None
-
 
 class AccountServiceTest(unittest.TestCase):
 
     account = Account("Test Account", balance=4000)
 
-    def test_create(self):
+    def test_00_create(self):
         self.assertTrue(AccountService.create_account(AccountServiceTest.account, 1))
 
-    def test_get_all(self):
+    def test_01_get_all(self):
         accounts = AccountService.get_accounts_by_client(1)
         AccountServiceTest.account = Account.deserialize(accounts.pop())
-        self.assertFalse(accounts)
+        self.assertTrue(accounts)
 
-    def test_get_one(self):
+    def test_02_get_one(self):
         self.assertTrue(AccountService.get_account(1, AccountServiceTest.account.id))
 
-    def test_update(self):
+    def test_03_update(self):
         account = Account("Updated Test Account", balance=4000)
         response = AccountService.update_account(1, AccountServiceTest.account.id, account)
-        self.assertEqual(response[1], 404)
+        self.assertEqual(response[1], 201)
 
-    def test_withdraw(self):
+    def test_04_withdraw(self):
         response = AccountService.withdrawal_account(1, AccountServiceTest.account.id, 400)
         self.assertEqual(response[1], 200)
 
-    def test_deposit(self):
+    def test_05_deposit(self):
         response = AccountService.deposit_account(1, AccountServiceTest.account.id, 400)
         self.assertEqual(response[1], 200)
 
-    def test_transfer(self):
+    def test_06_transfer(self):
         response = AccountService.transfer_account(1, AccountServiceTest.account.id,
                                                    AccountServiceTest.account.id, 400)
         self.assertEqual(response[1], 200)
 
-    def test_delete(self):
+    def test_07_delete(self):
         response = AccountService.delete_account(1, AccountServiceTest.account.id)
-        self.assertEqual(response[1], 200)
+        self.assertEqual(response[1], 204)
 
 
 if __name__ == '__main__':
